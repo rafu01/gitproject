d71cda3a3fbebf3da0e58f1a61d327ae63abd5ee
rafu01 <nrafsan13@gmail.com>
2020-09-26 21:48:37 +0600

    first

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("my title"),
+            centerTitle: true,
+          ),
+          body: Column(
+            children: [
+              Row(children: [
+                Text(
+                  "1. github ",
+                  style: TextStyle(fontSize: 20),
+                ),
+                Text(
+                  "2.gitbash ",
+                  style: TextStyle(fontSize: 20),
+                ),
+                Text(
+                  "2.gitbash",
+                  style: TextStyle(fontSize: 20),
+                ),
+                Text(
+                  "2.gitbash",
+                  style: TextStyle(fontSize: 20),
+                )
+              ]),
+            ],
+          ),
+        ));
+  }
+}
