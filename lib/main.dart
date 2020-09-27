6e3ef40fc7ca9cf18f283245dfbf877c5e6a3253
rafu01 <nrafsan13@gmail.com>
2020-09-27 12:09:34 +0600

    async function added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:gitproject/model/TechCrunchNewsModel.dart';
 
 void main() {
   runApp(MyApp());
@@ -22,6 +26,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List<Articles> data = List<Articles>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,8 +35,31 @@ class _MyHomePageState extends State<MyHomePage> {
         centerTitle: true,
       ),
       body: ListView(
-        children: [],
+        children: [
+          FutureBuilder(
+              future: getData(),
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Text('Loading');
+                } else if (snapshot.connectionState == ConnectionState.done) {
+                  return Text('data recieved');
+                }
+              }),
+        ],
       ),
     );
   }
+}
+
+getData() async {
+  try {
+    http.Response response = await http.get(
+        "http://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=6669c42118fb40ab8a0a88aa4dfddf2a");
+    Map<String, dynamic> map = jsonDecode(response.body);
+    List<dynamic> articles = map["articles"];
+    for (int i = 0; i < articles.length; i++) {
+      Articles ar = Articles.fromJson(articles[i]);
+      print(ar.title);
+    }
+  } catch (e) {}
 }
